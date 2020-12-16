cbace37300d012741c5e60f53b9429b431a699da
Sean <skabe89@gmail.com>
2020-12-16 12:56:54 -0500

    commit tree 02b481608d4b1a9e1d42d0b2bc100eda8fea124e

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get ('/') do
+        "Find out about ya boi"
+    end
+
+    get('/name') do
+        "My name is Lani"
+    end
+
+    get('/hometown') do
+        "My hometown is Greensboro, NC"
+    end
+
+    get('/favorite-song') do
+        "My favorite song is 'Black Skinhead' by Kanye"
+    end
+
 end
